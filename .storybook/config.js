2c7f4e4676a2cdab5fd8f13b8dc3c8441ee073cd
Ash Johns <ashelypjohns@gmail.com>
2019-09-30 13:35:47 -0400

    Add Accessibility testing addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '../src/css/variables.css';
 import '../src/css/typography.css';
+import { withA11y } from '@storybook/addon-a11y';
+
+addDecorator(withA11y);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src', true, /\.stories\.js$/), module);
